8732c31424f22933ea97febc0b6fc2759df6785d
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-09-29 18:32:12 +0100

    added new functions for repl

--- lox/Lox.java
+++ lox/Lox.java
@@ -1,8 +1,12 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.List;
 
 
 
@@ -23,5 +27,29 @@ public class Lox{
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
 
+    }
+
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if(line == null){
+                break;
+            }
+            run(line);
+        }
+    }
+
+    private static void run(String source) throws IOException{
+        Scanner scanner = new Scanner(source);
+
+        List<Token> tokens = scanner.ScanTokens();
+
+        for(Token token : tokens){
+            System.out.println(token);
+        }
     }
 }
