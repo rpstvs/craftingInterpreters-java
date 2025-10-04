328a1b8fe8653bb0805bf638a8f97543188db06e
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-10-04 11:32:44 +0100

    changes on scanner

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -28,6 +28,8 @@ public class Scanner {
     private void scanToken(){
         char c = advance();
 
+
+
         switch (c) {
             case '(': addToken(TokenType.LEFT_PAREN); break;
             case ')': addToken(TokenType.RIGHT_PAREN); break;
@@ -39,7 +41,22 @@ public class Scanner {
             case '+': addToken(TokenType.PLUS); break;
             case ';': addToken(TokenType.SEMICOLON); break;
             case '*': addToken(TokenType.STAR); break;
+            case '!':addToken(match('=')? TokenType.BANG_EQUAL : TokenType.BANG); break;
+            case '=':addToken(match('=')? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+            case '<':addToken(match('=')? TokenType.LESS_EQUAL : TokenType.LESS); break;
+            case '>':addToken(match('=')? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+            default:
+            Lox.error(line, "Unexpected character");
+            break;
         }
+    }
+
+    private boolean match(char expected){
+        if(isAtEnd()) return false;
+
+        if(source.charAt(current) != expected) return false;
+        current++;
+        return true;
     }
 
     private boolean isAtEnd(){
