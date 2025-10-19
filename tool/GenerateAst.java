8e579f3bcb93108bfdf265001841c27a968d76bf
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-10-19 12:54:02 +0100

    changes on AST

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -1,6 +1,9 @@
 package tool;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class GenerateAst {
     public static void main(String[] args) throws IOException {
@@ -9,5 +12,26 @@ public class GenerateAst {
             System.exit(64);
         }
         String outputDir = args[0];
+
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal : Object Value",
+            "Unary : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String basename, List<String> types) throws IOException{
+
+        String path = outputDir + "/" + basename + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class "+ basename + " {");
+        writer.println("}");
+        writer.close();
     }
 }
