629a5397ee41bb8cbf17f9da6926d2c6fbefd2f6
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-11-20 12:52:40 +0000

    added parser

--- lox/Parser.java
+++ lox/Parser.java
@@ -10,5 +10,29 @@ public class Parser {
         this.tokens = tokens;
     }
 
+    private Expr expression(){
+        return equality();
+    }
+
+    private Expr equality(){
+        Expr expr = comparison();
+
+        while(match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)){
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right)
+        }
+        return expr;
+    }
+
+    private boolean match(TokenType... types){
+        for (TokenType type: types){
+            if(check(type)){
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
     
 }
